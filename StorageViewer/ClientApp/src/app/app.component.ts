550e84b72c27dac6b0703851cd64fb41c365d8c8
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-09-19 13:41:06 +0300

    Animation for view changing process

--- StorageViewer/ClientApp/src/app/app.component.ts
+++ StorageViewer/ClientApp/src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
 
 import { DuplexChannelService } from './common/duplex-channel.service';
+import { routeSlideAnimation } from './animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [routeSlideAnimation]
 })
 export class AppComponent {
   title = 'ClientApp';
 
-  constructor(private serverChannel: DuplexChannelService) {}
+  constructor(private serverChannel: DuplexChannelService, private routerContexts: ChildrenOutletContexts) {}
 
   ngOnInit() {
     this.serverChannel.ensureConnect();
+  }
+
+  getRouteAnimationData() {
+    return this.routerContexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
   }
 }
--- StorageViewer/ClientApp/src/app/app.routes.ts
+++ StorageViewer/ClientApp/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { ActualInfoPageComponent } from './views/actual-info-page.component';
 import { HistoryPageComponent } from './views/history-page.component';
+import { animation } from '@angular/animations';
 
 export const routes: Routes = [
-    { path: 'actual', title: 'Actual Weather Data', component: ActualInfoPageComponent },
-    { path: 'history', title: 'Historical Weather Data', component: HistoryPageComponent },
+    { path: 'actual', title: 'Actual Weather Data', component: ActualInfoPageComponent, data: { animation: 'actual' } },
+    { path: 'history', title: 'Historical Weather Data', component: HistoryPageComponent, data: { animation: 'history' } },
     { path: '', redirectTo: '/actual', pathMatch: 'full' }
 ];
