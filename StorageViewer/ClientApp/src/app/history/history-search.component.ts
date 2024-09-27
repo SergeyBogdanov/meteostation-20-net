d28820077306f27e26e2285ff8e720f11a3ebece
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-09-27 17:47:37 +0300

    Add switch to view grid or chart

--- StorageViewer/ClientApp/src/app/history/history-search.component.ts
+++ StorageViewer/ClientApp/src/app/history/history-search.component.ts
@@ -4,17 +4,19 @@ import { HistoryFilterOptionsModel } from "./shared/history-filter-options.model
 import { MeteoDataItemModel } from "./shared/meteo-data-item.model";
 import { HistoryListComponent } from "./history-list.component";
 import { HistoryChartComponent } from "./history-chart.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'history-search',
     standalone: true,
-    imports: [HistoryListComponent, HistoryChartComponent],
+    imports: [HistoryListComponent, HistoryChartComponent, FormsModule],
     templateUrl: 'history-search.component.html',
     styleUrl: 'history-search.component.css'
 })
 export class HistorySearchComponent {
     historyRows: MeteoDataItemModel[] = [];
     actionInProgress: boolean = false;
+    isTableShown: boolean = true;
     @Output() changeSearchStatus = new EventEmitter<boolean>();
 
     constructor(private historyService: HistoryService) {}
@@ -27,6 +29,10 @@ export class HistorySearchComponent {
                                                 new Date((options.toDate ?? new Date()).getTime() + 24 * 60 * 60 * 1000));
             this.notifySearchStatus(false);
         }
+    }
+
+    setTableView(viewTable:boolean) {
+        this.isTableShown = viewTable;
     }
 
     private notifySearchStatus(isSearching: boolean): void {
