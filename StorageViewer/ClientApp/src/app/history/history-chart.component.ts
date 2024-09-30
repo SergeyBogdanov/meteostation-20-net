7977b848120e79d44a325704418c300955229466
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-09-30 13:26:03 +0300

    Show some data in the chart

--- StorageViewer/ClientApp/src/app/history/history-chart.component.ts
+++ StorageViewer/ClientApp/src/app/history/history-chart.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { BaseChartDirective } from "ng2-charts";
+
+import { MeteoDataItemModel } from "./shared/meteo-data-item.model";
 
 @Component({
     selector: 'history-chart',
@@ -9,9 +11,30 @@ import { BaseChartDirective } from "ng2-charts";
     styleUrl: 'history-chart.component.css'
 })
 export class HistoryChartComponent {
+    private originalHistoryRows: MeteoDataItemModel[] = [];
+    @Input() get historyRows(): MeteoDataItemModel[] {
+        return this.originalHistoryRows;
+    }
+    set historyRows(val: MeteoDataItemModel[]) {
+        this.originalHistoryRows = val;
+        this.chartData = {
+            datasets: [{
+                data: (val ?? []).map(item => ({x: item.recordTimestamp, y: item.storedData?.pressureMmHg}))
+            }]
+        }
+    }
+
     chartData: any = {
         datasets: [{
             data: [{x: '1', y: 3}, {x: '2', y: 5}, {x: '3', y: 7}]
         }]
+    };
+
+    chartOptions: any = {
+        // scales: {
+        //     yAxis: {
+        //         type: "time"
+        //     }
+        // }
     };
 }
