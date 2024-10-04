222d8bb0f191a1d8492ec202d70479946b52363e
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-10-04 17:27:59 +0300

    Add pressure chart to front page

---
+++ StorageViewer/ClientApp/src/app/controls/date-axis-chart.component.ts
@@ -0,0 +1,67 @@
+import { Component, EventEmitter, Input, Output, SimpleChanges } from "@angular/core";
+import { BaseChartDirective } from "ng2-charts";
+import "chartjs-adapter-moment";
+
+@Component({
+    selector: 'date-axis-chart',
+    standalone: true,
+    imports: [BaseChartDirective],
+    templateUrl: 'date-axis-chart.component.html',
+    styleUrl: 'date-axis-chart.component.css'
+})
+export class DateAxisChartComponent {
+    @Input() data: number[] = [];
+    @Input() labels: Date[] | string[] = [];
+    @Input() type: 'line' | 'bar' = 'line';
+    @Output() click = new EventEmitter();
+    chartData: any = {
+        datasets: [{
+            label: false,
+            data: [{x: '1', y: 2}, {x: '2', y: 5}, {x: '3', y: 1}],
+            yAxisID: 'yAxis'
+        }]
+    }
+    chartOptions: any = {
+        scales: {
+            xAxis: {
+                type: "time"
+            },
+            yAxis: {}
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        elements: {
+            point : {
+                radius: 0
+            }
+        }
+    };
+
+    ngOnChanges(changes: SimpleChanges) {
+        let extractedData: any[] = [];
+        let minAxisValue: number | undefined = undefined;
+        if (this.data?.length > 0 && this.data?.length === this.labels?.length) {
+            extractedData = this.data.map((item, index) => ({ x: this.labels[index], y: item}));
+            if (this.type === 'bar') {
+                const numericData = this.data.filter(item => !Number.isNaN(item) && item > 0);
+                minAxisValue = Math.min(...numericData);
+                if (Number.isFinite(minAxisValue)) {
+                    const minValuePadding = ((Math.max(...numericData) - minAxisValue) * 0.10) ?? 10;
+                    minAxisValue -= minValuePadding;
+                }
+            }
+        }
+        this.chartData = {
+            datasets: [{
+                label: false,
+                data: extractedData,
+                yAxisID: 'yAxis'
+            }]
+        };
+        this.chartOptions.scales['yAxis'] = (minAxisValue !== undefined && Number.isFinite(minAxisValue)) ?
+                                                { min: minAxisValue } : {};
+    }
+}
--- StorageViewer/ClientApp/src/app/views/actual-info-page.component.ts
+++ StorageViewer/ClientApp/src/app/views/actual-info-page.component.ts
@@ -2,32 +2,40 @@ import { Component } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { numberChangeAnimation } from '../animations';
+import moment from 'moment';
 
 import { MeteoDataItemFactory } from '../history/shared/meteo-data-item.factory';
 import { DuplexChannelService } from '../common/duplex-channel.service';
 import { MeteoDataItemModel } from '../history/shared/meteo-data-item.model';
 import { DateFormattingPipe } from '../controls/date-formatting.pipe';
-import { numberChangeAnimation } from '../animations';
 import { AirQualityDirective } from '../controls/air-quality.directive';
 import { AirQualityComponent } from '../controls/air-quality.component';
+import { DateAxisChartComponent } from '../controls/date-axis-chart.component';
+import { HistoryService } from '../history/shared/history.service';
 
 @Component({
   selector: 'actual-info-page',
   standalone: true,
-  imports: [RouterLink, DateFormattingPipe, DecimalPipe, AirQualityDirective, AirQualityComponent],
+  imports: [RouterLink, DateFormattingPipe, DecimalPipe, AirQualityDirective, AirQualityComponent, DateAxisChartComponent],
   templateUrl: 'actual-info-page.component.html',
   styleUrl: 'actual-info-page.component.css',
   animations: [numberChangeAnimation]
 })
 export class ActualInfoPageComponent {
     currentInfo?: MeteoDataItemModel = undefined;
+    latestPressureData: number[] = [];
+    pressureDataLabels: string[] = [];
+
     private actualSubscription?: Subscription;
 
-    constructor(private meteoInfoFactory: MeteoDataItemFactory, private serverChannel: DuplexChannelService) {}
+    constructor(private meteoInfoFactory: MeteoDataItemFactory, private serverChannel: DuplexChannelService, private historyService: HistoryService) {}
 
-    ngOnInit() {
+    async ngOnInit() {
         this.actualSubscription = this.serverChannel.dataReceived.subscribe((data) => this.consumeWebSocketInfo(data));
         this.serverChannel.ensureConnect();
+        const historycalData = await this.requestLatestData();
+        this.aggregatePressureData(historycalData);
     }
 
     ngOnDestroy() {
@@ -36,5 +44,14 @@ export class ActualInfoPageComponent {
 
     private consumeWebSocketInfo(info: any) {
         this.currentInfo = this.meteoInfoFactory.restoreFromServerObject(info);
+    }
+
+    private async requestLatestData(): Promise<MeteoDataItemModel[]> {
+        return await this.historyService.getHistory(moment().subtract(12, 'hours').toDate(), moment().toDate());
+    }
+
+    private aggregatePressureData(dataBlock: MeteoDataItemModel[]) {
+        this.latestPressureData = dataBlock.map(item => item.storedData?.pressureMmHg ?? 0);
+        this.pressureDataLabels = dataBlock.map(item => item.recordTimestamp);
     }
 };
